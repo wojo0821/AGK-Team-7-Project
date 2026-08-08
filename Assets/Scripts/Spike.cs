15d9b0fd8fad9b36c84ba58c9d4c262a176940ee
wojo0821 <wojo0821@gmail.com>
2026-08-08 14:39:23 +0900

    feat: 프리팹 타일맵에 추가

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -4,8 +4,7 @@ public class Spike : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        PlayerRespawn playerRespawn =
-            other.GetComponentInParent<PlayerRespawn>();
+        PlayerRespawn playerRespawn = other.GetComponentInParent<PlayerRespawn>();
 
         if (playerRespawn != null)
         {
