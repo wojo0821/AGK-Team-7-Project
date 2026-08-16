4ca314ab20beecad8845be385e652fa6424001e4
wojo0821 <wojo0821@gmail.com>
2026-08-16 19:27:22 +0900

    feat: 레이저스포너 추가(상시 켜짐만 완성)

---
+++ Assets/01_Scripts/LazerSpawner.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class LazerSpawner : MonoBehaviour
+{
+    [Header("Lazer")]
+    [SerializeField] private GameObject lazer;
+    [Header("Lazer Settings")]
+    [SerializeField] private bool isAlwaysOn = true;
+    [SerializeField] private float onTime = 2f;
+    [SerializeField] private float offTime = 2f;
+    [SerializeField] private Vector2 lazerSize;
+    [SerializeField] private float zDirection;
+    
+    private void Start()
+    {
+        GameObject newLazer = Instantiate(lazer, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.Euler(0, 0, zDirection));
+        newLazer.transform.localScale = new Vector3(lazerSize.x, lazerSize.y, 1);
+    } 
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        // 1. 기존 기즈모 좌표계 백업
+        Matrix4x4 oldMatrix = Gizmos.matrix;
+        // 2. 현재 오브젝트 위치 + zDirection 회전 적용된 좌표계로 설정
+        Gizmos.matrix = Matrix4x4.TRS(transform.position, Quaternion.Euler(0, 0, zDirection), Vector3.one);
+        // 3. 로컬 좌표 기준 (가로길이 / 2) 만큼 오른쪽으로 중심 이동
+        Vector3 center = new Vector3(lazerSize.x / 2f, 0f, 0f);
+        Gizmos.DrawWireCube(center, lazerSize - new Vector2(0, 0.75f)); //레이저 크기 일치를 위해 -0.75
+        // 4. 좌표계 원상복구
+        Gizmos.matrix = oldMatrix;
+    }
+}
