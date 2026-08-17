acea105cac54c7f67ff03228058ab4f977cff568
wojo0821 <wojo0821@gmail.com>
2026-08-17 13:09:43 +0900

    feat: 레이저 타이머 추가 및 회전 추가

--- Assets/01_Scripts/LazerSpawner.cs
+++ Assets/01_Scripts/LazerSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class LazerSpawner : MonoBehaviour
@@ -6,16 +7,52 @@ public class LazerSpawner : MonoBehaviour
     [SerializeField] private GameObject lazer;
     [Header("Lazer Settings")]
     [SerializeField] private bool isAlwaysOn = true;
+    [SerializeField] private bool isAutoRotation = false;
+    [SerializeField] private float rotationSpeed = 90f;
     [SerializeField] private float onTime = 2f;
     [SerializeField] private float offTime = 2f;
     [SerializeField] private Vector2 lazerSize;
     [SerializeField] private float zDirection;
-    
+
+    private GameObject newLazer;
+
     private void Start()
     {
-        GameObject newLazer = Instantiate(lazer, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.Euler(0, 0, zDirection));
+        if (isAlwaysOn)
+        {
+            LazerOn();
+        }
+        else
+        {
+            StartCoroutine(LazerRoutine());
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (isAutoRotation)
+        {
+            newLazer.transform.Rotate(0, 0, rotationSpeed * Time.fixedDeltaTime);
+        }
+    }
+    IEnumerator LazerRoutine()
+    {
+        while (true)
+        {
+            LazerOn();
+            yield return new WaitForSeconds(onTime);
+            LazerOff();
+            yield return new WaitForSeconds(offTime);
+        }
+    }
+    private void LazerOn()
+    {
+        newLazer = Instantiate(lazer, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.Euler(0, 0, zDirection));
         newLazer.transform.localScale = new Vector3(lazerSize.x, lazerSize.y, 1);
-    } 
+    }
+    private void LazerOff()
+    {
+        Destroy(newLazer);
+    }
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
